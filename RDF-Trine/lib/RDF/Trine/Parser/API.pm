e8807b51ddc4af346e9d5e9a3c1e63c350c00e3d
Konstantin Baierer <konstantin.baierer@gmail.com>
2012-08-29 14:23:45 +0200

    Interface roles for Parsers and Serializers

---
+++ RDF-Trine/lib/RDF/Trine/Parser/API.pm
@@ -0,0 +1,38 @@
+package RDF::Trine::Parser::API;
+use Moose::Role;
+
+requires (
+    '_parse_graph',
+    '_parse_bindings',
+    'media_type',
+);
+
+1;
+
+__END__
+
+=head1 NAME
+
+RDF::Trine::Parser::API - Interface Role for Parsers
+
+=head1 DESCRIPTION
+
+Every Parser needs to implement
+
+=over 4
+
+=item media_type
+
+A constant array of supported media types, used for linking parsers to formats
+
+=item _parse_bindings
+
+Takes a filehandle and parses it into bindings iterator
+
+=item _parse_graph
+
+Takes a filehandle and parses it into a graph iterator
+
+=back
+
+=cut
---
+++ RDF-Trine/lib/RDF/Trine/Serializer/API.pm
@@ -0,0 +1,38 @@
+package RDF::Trine::Serializer::API;
+use Moose::Role;
+
+requires (
+    '_serialize_graph',
+    '_serialize_bindings',
+    'media_types',
+);
+
+1;
+
+__END__
+
+=head1 NAME
+
+RDF::Trine::Serializer::API - Interface Role for Serializers
+
+=head1 DESCRIPTION
+
+Every Serializer needs to implement
+
+=over 4
+
+=item media_type
+
+A constant array of supported media types, used for linking serializers to formats
+
+=item _serialize_bindings
+
+Takes a binding iterator and serializes it to a filehandle
+
+=item _serialize_graph
+
+Takes a graph iterator and serializes it to a filehandle
+
+=back
+
+=cut
