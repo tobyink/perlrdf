12c74ecff601848d1f89cc10f57f969ff98f79ea
Toby Inkster <tobyink@cpan.org>
2012-08-29 14:30:05 +0100

    port NTriples serializer to new API

--- RDF-Trine/lib/RDF/Trine/Serializer/NTriples.pm
+++ RDF-Trine/lib/RDF/Trine/Serializer/NTriples.pm
@@ -1,217 +1,79 @@
-# RDF::Trine::Serializer::NTriples
-# -----------------------------------------------------------------------------
-
-=head1 NAME
-
-RDF::Trine::Serializer::NTriples - N-Triples Serializer
-
-=head1 VERSION
-
-This document describes RDF::Trine::Serializer::NTriples version 1.000
-
-=head1 SYNOPSIS
-
- use RDF::Trine::Serializer::NTriples;
- my $serializer	= RDF::Trine::Serializer::NTriples->new();
-
-=head1 DESCRIPTION
-
-The RDF::Trine::Serializer::NTriples class provides an API for serializing RDF
-graphs to the N-Triples syntax.
-
-=head1 METHODS
-
-Beyond the methods documented below, this class inherits methods from the
-L<RDF::Trine::Serializer> class.
-
-=over 4
-
-=cut
-
 package RDF::Trine::Serializer::NTriples;
 
-use strict;
-use warnings;
-use base qw(RDF::Trine::Serializer);
+use constant media_types => [qw( text/plain )];
+use RDF::Trine::FormatRegistry -register_serializer;
 
-use URI;
-use Carp;
-use Data::Dumper;
-use Scalar::Util qw(blessed);
+use Moose;
+with qw(
+	RDF::Trine::Serializer::API
+);
 
-use RDF::Trine;
-use RDF::Trine::Statement::Triple;
-use RDF::Trine::Error qw(:try);
-
-######################################################################
-
-our ($VERSION);
-BEGIN {
-	$VERSION	= '1.000';
-	$RDF::Trine::Serializer::serializer_names{ 'ntriples' }	= __PACKAGE__;
-	$RDF::Trine::Serializer::format_uris{ 'http://www.w3.org/ns/formats/N-Triples' }	= __PACKAGE__;
-	foreach my $type (qw(text/plain)) {
-		$RDF::Trine::Serializer::media_types{ $type }	= __PACKAGE__;
-	}
-}
-
-######################################################################
-
-=item C<< new >>
-
-Returns a new N-Triples serializer object.
-
-=cut
-
-sub new {
-	my $class	= shift;
-	my %args	= @_;
-	my $self = bless( {}, $class);
-	return $self;
-}
-
-=item C<< serialize_model_to_file ( $fh, $model ) >>
-
-Serializes the C<$model> to N-Triples, printing the results to the supplied
-filehandle C<<$fh>>.
-
-=cut
-
-sub serialize_model_to_file {
-	my $self	= shift;
-	my $file	= shift;
-	my $model	= shift;
-	my $st		= RDF::Trine::Statement::Triple->new( map { RDF::Trine::Node::Variable->new($_) } qw(s p o) );
-	my $pat		= RDF::Trine::Pattern->new( $st );
-	my $stream	= $model->get_pattern( $pat, undef, orderby => [ qw(s ASC p ASC o ASC) ] );
-	my $iter	= $stream->as_statements( qw(s p o) );
+sub _serialize_graph {
+	my ($self, $iter, $fh) = @_;
 	while (my $st = $iter->next) {
-		print {$file} $self->statement_as_string( $st );
+		print {$fh} $self->statement_as_string($st);
 	}
 }
 
-=item C<< serialize_model_to_string ( $model ) >>
+sub _serialize_bindings {
+	confess "cannot handle bindings";
+}
 
-Serializes the C<$model> to N-Triples, returning the result as a string.
-
-=cut
-
-sub serialize_model_to_string {
-	my $self	= shift;
-	my $model	= shift;
-	my $st		= RDF::Trine::Statement::Triple->new( map { RDF::Trine::Node::Variable->new($_) } qw(s p o) );
-	my $pat		= RDF::Trine::Pattern->new( $st );
-	my $stream	= $model->get_pattern( $pat, undef, orderby => [ qw(s ASC p ASC o ASC) ] );
-	my $iter	= $stream->as_statements( qw(s p o) );
+sub model_to_file
+{
+	my ($self, $model, $fh) = @_;
+	$fh = $self->_ensure_fh($fh);
 	
-	my $string	= '';
+	my $st     = RDF::Trine::Statement::Triple->new( map { RDF::Trine::Node::Variable->new($_) } qw(s p o) );
+	my $pat    = RDF::Trine::Pattern->new( $st );
+	my $stream = $model->get_pattern( $pat, undef, orderby => [ qw(s ASC p ASC o ASC) ] );
+	my $iter	  = $stream->as_statements( qw(s p o) );
+	
 	while (my $st = $iter->next) {
-		my @nodes	= $st->nodes;
-		$string		.= $self->statement_as_string( $st );
-	}
-	return $string;
-}
-
-=item C<< serialize_iterator_to_file ( $file, $iter ) >>
-
-Serializes the iterator to N-Triples, printing the results to the supplied
-filehandle C<<$fh>>.
-
-=cut
-
-sub serialize_iterator_to_file {
-	my $self	= shift;
-	my $file	= shift;
-	my $iter	= shift;
-	while (my $st = $iter->next) {
-		print {$file} $self->statement_as_string( $st );
+		print {$fh} $st->as_ntriples;
 	}
 }
 
-=item C<< serialize_iterator_to_string ( $iter ) >>
-
-Serializes the iterator to N-Triples, returning the result as a string.
-
-=cut
-
-sub serialize_iterator_to_string {
-	my $self	= shift;
-	my $iter	= shift;
-	my $string	= '';
-	while (my $st = $iter->next) {
-		my @nodes	= $st->nodes;
-		$string		.= $self->statement_as_string( $st );
-	}
-	return $string;
-}
+sub serialize_node      { $_[1]->as_ntriples }
+sub statement_as_string { $_[1]->as_ntriples }
 
 sub _serialize_bounded_description {
-	my $self	= shift;
-	my $model	= shift;
-	my $node	= shift;
-	my $seen	= shift || {};
-	return '' if ($seen->{ $node->sse }++);
-	my $iter	= $model->get_statements( $node, undef, undef );
-	my $string	= '';
+	my ($self, $model, $node, $seen) = @_;
+	$seen ||= {};
+	return '' if $seen->{ $node->sse }++;
+	
+	my $iter   = $model->get_statements( $node, undef, undef );
+	my $string = '';
 	while (my $st = $iter->next) {
-		my @nodes	= $st->nodes;
-		$string		.= $self->statement_as_string( $st );
-		if ($nodes[2]->isa('RDF::Trine::Node::Blank')) {
-			$string	.= $self->_serialize_bounded_description( $model, $nodes[2], $seen );
+		$string .= $st->as_ntriples;
+		if ($st->object->is_blank) {
+			$string .= $self->_serialize_bounded_description($model, $st->object, $seen);
 		}
 	}
 	return $string;
 }
 
-=item C<< statement_as_string ( $st ) >>
-
-Returns a string with the supplied RDF::Trine::Statement object serialized as
-N-Triples, ending in a DOT and newline.
-
-=cut
-
-sub statement_as_string {
-	my $self	= shift;
-	my $st		= shift;
-	my @nodes	= $st->nodes;
-	return join(' ', map { $_->as_ntriples } @nodes[0..2]) . " .\n";
-}
-
-=item C<< serialize_node ( $node ) >>
-
-Returns a string containing the N-Triples serialization of C<< $node >>.
-
-=cut
-
-sub serialize_node {
-	my $self	= shift;
-	my $node	= shift;
-	return $node->as_ntriples;
-}
-
+__PACKAGE__->meta->make_immutable;
 1;
 
-__END__
+=head1 NAME
+
+RDF::Trine::Serializer::NTriples - write data in N-Triples format
+
+=head DESCRIPTION
+
+This serializer implements L<RDF::Trine::Serializer::API>.
+
+=head2 Methods
+
+This module provides the following additional methods.
+
+=over
+
+=item C<< serialize_node($node) >>
+
+=item C<< statement_as_string($st) >>
 
 =back
 
-=head1 BUGS
 
-Please report any bugs or feature requests to through the GitHub web interface
-at L<https://github.com/kasei/perlrdf/issues>.
-
-=head1 SEE ALSO
-
-L<http://www.w3.org/TR/rdf-testcases/#ntriples>
-
-=head1 AUTHOR
-
-Gregory Todd Williams  C<< <gwilliams@cpan.org> >>
-
-=head1 COPYRIGHT
-
-Copyright (c) 2006-2012 Gregory Todd Williams. This
-program is free software; you can redistribute it and/or modify it under
-the same terms as Perl itself.
-
-=cut
